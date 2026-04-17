5f8ca34d1468b70ed28442d79a6ebc5007352077
20Alex16 <tripaa.alex@gmail.com>
2026-04-17 11:16:21 +0300

    12/60 better not found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,9 +1,24 @@
+import { Button } from 'react-bootstrap'
+import './NotFound.css'
 
 export default function NotFound() {
     return (
-        <div>
-            <h1>404</h1>
-            <h2>Pagina nu a fost gasita</h2>
+        <div className='notfound-section'>
+            <div className='notfound-overlay'></div>
+            <div className='notfound-content'>
+                <div className='sad-face'>:-( </div>
+                <h1 className='notfound-title'>404</h1>
+                <h2 className='notfound-subtitle'>Pagina nu a fost gasita</h2>
+                <p className='notfound-description'>
+                    Ne pare rau, pagina pe care o cauti nu exista.
+                </p>
+                <Button 
+                    className='btn-orange btn-glow'
+                    href="/"
+                >
+                    Mergi la pagina acasa
+                </Button>
+            </div>
         </div>
     )
 }
