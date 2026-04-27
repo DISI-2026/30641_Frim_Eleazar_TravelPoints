3485e8e30415f3f096a33e4b9730a56b74fbc2a2
20Alex16 <tripaa.alex@gmail.com>
2026-04-27 12:24:58 +0300

    7/68 am adugat posibilitatea de a filtra si cauta obiective

--- frontend/src/pages/AttractionsPage.tsx
+++ frontend/src/pages/AttractionsPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Container, ListGroup, Modal, Spinner } from "react-bootstrap";
+import { Button, Card, Container, Form, ListGroup, Modal, Spinner } from "react-bootstrap";
 import { deleteAttraction, getAttractions, updateAttraction, type AttractionType } from '../API/attraction_api';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from "react";
@@ -24,9 +24,16 @@ const DeleteModal = ({ show, onHide, onConfirm }: { show: boolean; onHide: () =>
     </Modal>
 );
 
+type LocationFilterType = "Italia" | "Romania" | "Franta" | "Spania" | undefined
+type AttractionFilter = {
+    location: LocationFilterType,
+    name: string
+}
+
 export default function AttractionsPage() {
     const [confirmDelete, setConfirmDelete] = useState(false);
     const [attractionToEdit, setAttractionToEdit] = useState<AttractionType | null>(null);
+    const [attractionFilter, setAttractionFilter] = useState<AttractionFilter>({ location: undefined, name: "" });
 
     const { data: attractions, isError, error, isLoading } = useQuery({
         queryKey: ["attractions"],
@@ -98,8 +105,43 @@ export default function AttractionsPage() {
 
             <h1>Atracții</h1>
             <Button className="btn-orange btn-slim" href="/newattraction">Creeza o noua atractie</Button>
+            
+            <Form>
+                <Form.Group className="mb-3" controlId="attractionName">
+                    <Form.Label>Caută după nume</Form.Label>
+                    <Form.Control type="text" placeholder="Numele atractiei" 
+                        defaultValue={attractionFilter.name}
+                        onChange={(e) => {
+                            setAttractionFilter({ ...attractionFilter, name: e.target.value })
+                        }}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="location">
+                    <Form.Label>Filtrează după locație</Form.Label>
+                    <Form.Select aria-label="Default select example"
+                        defaultValue={attractionFilter.location}
+                        onChange={(e) => {
+                            const val = e.target.value as LocationFilterType
+                            setAttractionFilter({ ...attractionFilter, location: val })
+                        }}
+                    >
+                        <option value="">Selectează o locație</option>
+                        <option value="Italia">Italia</option>
+                        <option value="Romania">Romania</option>
+                        <option value="Franta">Franta</option>
+                        <option value="Spania">Spania</option>
+                    </Form.Select>
+                </Form.Group>
+            </Form>
+
             <ListGroup>
-                {attractions.map((attraction, index) => (
+                {attractions.filter((att) => {
+                    return (
+                        (attractionFilter.name ? att.name.toLowerCase().includes(attractionFilter.name.toLowerCase()) : true) &&
+                        (attractionFilter.location ? att.location === attractionFilter.location : true)
+                    );
+                }).map((attraction, index) => (
                     <ListGroup.Item key={index}>
                         <Card>
                             <Card.Body>
